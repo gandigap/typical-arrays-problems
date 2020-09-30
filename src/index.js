ba785b755bcc712f2a8922d7ed375c8e2dfe3425
Ihar Berasneu <igor.simatic@gmail.com>
2020-09-30 23:25:12 +0300

    feat:add code for functions min max and average

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return (typeof array == "undefined" || array.length <= 0) ? 0
+    : array.reduce((min, current) => {
+      return (min < current) ? min : current;
+    });
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return (typeof array == "undefined" || array.length <= 0) ? 0
+    : array.reduce((max, current) => {
+      return (max > current) ? max : current;
+    });
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (typeof array == "undefined" || array.length <= 0) {
+    return 0;
+  } else {
+    let total = 0;
+    let average = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+    return average = total / array.length;
+  }
 }
